c4146b920c9031c5820b81b2aef07fa5f7f0d1e2
baddystar12 <72180686+baddystar12@users.noreply.github.com>
2021-07-28 07:26:28 -0700

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -1,37 +1,71 @@
-var hr = hour();
-var min = minute();
-var sec = second();
-var minAngle, hrAngle, secAngle;
-function setup() {
-  createCanvas(800,400);
-  angleMode(DEGREES);
-  hrAngle = map(hr, 0, 60, 0, 360);
-  minAngle = map(min, 0, 60, 0, 360);
-  secAngle = map(sec, 0, 60, 0, 360);
-}
+var hr
+var mn
+var sc
+var hrAngle, mnAngle, scAngle
 
-function draw() {
-  background(255,255,255);  
-  ellipseMode(CENTER);
-  fill("white");
-  ellipse(400,200,300,300);
-  push();
-  rotate(hrAngle);
-  stroke("red");
-  strokeWeight(7);
-  line(400,200,100,100);
-  pop();
-  push();
-  rotate(minAngle);
-  stroke("royalblue");
-  strokeWeight(7);
-  line(400,200,75,75);
-  pop();
-  push();
-  rotate(secAngle);
-  stroke("lightgreen");
-  strokeWeight(7);
-  line(400,200,50,50);
-  pop();
-  drawSprites();
+function setup(){
+    createCanvas(500,657);
+    angleMode(DEGREES);  
+}
+function draw(){
+    background(0,0,0)
+
+    translate(200,200)
+    rotate(-90)
+
+    hr = hour()
+    mn = minute()
+    sc = second()
+    strokeWeight(8)
+    stroke(255, 150, 235)
+    noFill();
+    scAngle = map(sc, 0, 60, 0, 360)
+
+    strokeWeight(8)
+    stroke(154, 234, 101)
+    mnAngle = map(mn, 0, 60, 0, 360)
+
+    strokeWeight(8)
+    stroke(137, 96, 253)
+    hrAngle = map(hr % 12,  0, 12, 0, 360)
+
+
+    push();
+    rotate(scAngle)
+    stroke(255,0,0)
+    strokeWeight(7)
+    line(0,0,100,0)
+    pop()
+
+    push()
+    rotate(mnAngle)
+    stroke(0,255,0)
+    strokeWeight(7)
+    line(0,0,85,0)
+    pop()
+
+
+    push()
+    rotate(hrAngle)
+    stroke(0,0,255)
+    strokeWeight(7)
+    line(0,0,45,0)
+    pop()
+
+    strokeWeight(7)
+    noFill()
+
+    stroke(255,0,0)
+    arc(0,0,300,300,0,scAngle)
+
+    strokeWeight(7)
+    stroke(0,255,0);
+    arc(0,0,280,280,0,mnAngle)
+
+    strokeWeight(7)
+    stroke(0,0,255)
+    arc(0,0,260,260,0,hrAngle)
+
+    drawSprites();
+
 }
